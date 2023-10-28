7a60813791b214968c3ee796ee5e1b8c8b3ed460
Cailo Pinheiro <cailoisrael@hotmail.com>
2023-10-28 11:06:17 -0300

    build config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,24 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import svgLoader from 'vite-svg-loader'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), svgLoader()],
+  resolve: {
+    alias: {
+      // Defina alias para importar ícones ou imagens
+      '@icons': 'src/pages/Home/assets',
+      '@images': 'src/pages/Home/assets',
+    },
+  },
 })
